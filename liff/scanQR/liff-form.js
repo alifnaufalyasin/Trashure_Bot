cd8c90e81bcda0c317a146e6e2353e351a104202
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-06-22 23:03:15 +0700

    Update LIFF for scan QR barang

--- liff/scanQR/liff-form.js
+++ liff/scanQR/liff-form.js
@@ -15,11 +15,11 @@ function initializeLiff(myLiffId) {
     .then(async () => {
       const profile = liff.getContext()
       const userId = profile.userId
-      let data = { userId }
+      // let data = { userId }
       axios({
         url: "https://rpl-inventory.herokuapp.com/api/lineBot",
         method: "GET",
-        data: data,
+        data: { userId },
       })
       .then((dataToken) => {
         liff.scanCode()
