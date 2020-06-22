6c9b8599d5ef03c499137452e683c1971b3d0144
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-06-22 22:49:48 +0700

    Update LIFF for scan QR barang

--- liff/scanQR/liff-form.js
+++ liff/scanQR/liff-form.js
@@ -14,84 +14,98 @@ function initializeLiff(myLiffId) {
     })
     .then(async () => {
       const profile = liff.getContext()
-      let data = {}
-      data.userId = profile.userId
-      axios({
-        url: "https://rpl-inventory.herokuapp.com/api/lineBot",
-        method: "GET",
-        data: data,
-      })
-      .then((dataToken) => {
-        liff.scanCode()
-        .then((result) => {
-          const kode = result.value
-          const data2 = { kode }
-          axios({
-            url: "https://rpl-inventory.herokuapp.com/api/scanQR",
-            method: "POST",
-            headers: {
-              Authorization: `Bearer ${dataToken.data.data.token}`,
-            },
-            data: data2,
-          })
-            .then((response) => {
-              liff
-                .sendMessages([
-                  {
-                    type: "text",
-                    text: "Berhasil scan barang " + response.data.data.nama,
-                  },
-                ])
-                .then(() => {
-                  console.log("message sent")
-                  liff.closeWindow()
-                })
-                .catch((err) => {
-                  alert(err)
-                })
-            })
-            .catch((err) => {
-              liff
-                .sendMessages([
-                  {
-                    type: "text",
-                    text: "Error...",
-                  },
-                  {
-                    type: "text",
-                    text: err.data.data.message,
-                  },
-                ])
-                .then(() => {
-                  console.log("message sent")
-                  liff.closeWindow()
-                })
-                .catch((err) => {
-                  alert(err)
-                })
-            })
+      liff
+        .sendMessages([
+          {
+            type: "text",
+            text: profile.userId,
+          },
+        ])
+        .then(() => {
+          console.log("message sent")
+          liff.closeWindow()
         })
-      })
-      .catch((err) => {
-        liff
-          .sendMessages([
-            {
-              type: "text",
-              text: "Error...",
-            },
-            {
-              type: "text",
-              text: err.data.data.message,
-            },
-          ])
-          .then(() => {
-            console.log("message sent")
-            liff.closeWindow()
-          })
-          .catch((err) => {
-            alert(err)
-          })
-      })
+        .catch((err) => {
+          alert(err)
+        })
+      // let data = {}
+      // data.userId = profile.userId
+      // axios({
+      //   url: "https://rpl-inventory.herokuapp.com/api/lineBot",
+      //   method: "GET",
+      //   data: data,
+      // })
+      // .then((dataToken) => {
+      //   liff.scanCode()
+      //   .then((result) => {
+      //     const kode = result.value
+      //     const data2 = { kode }
+      //     axios({
+      //       url: "https://rpl-inventory.herokuapp.com/api/scanQR",
+      //       method: "POST",
+      //       headers: {
+      //         Authorization: `Bearer ${dataToken.data.data.token}`,
+      //       },
+      //       data: data2,
+      //     })
+      //       .then((response) => {
+      //         liff
+      //           .sendMessages([
+      //             {
+      //               type: "text",
+      //               text: "Berhasil scan barang " + response.data.data.nama,
+      //             },
+      //           ])
+      //           .then(() => {
+      //             console.log("message sent")
+      //             liff.closeWindow()
+      //           })
+      //           .catch((err) => {
+      //             alert(err)
+      //           })
+      //       })
+      //       .catch((err) => {
+      //         liff
+      //           .sendMessages([
+      //             {
+      //               type: "text",
+      //               text: "Error...",
+      //             },
+      //             {
+      //               type: "text",
+      //               text: err.data.data.message,
+      //             },
+      //           ])
+      //           .then(() => {
+      //             console.log("message sent")
+      //             liff.closeWindow()
+      //           })
+      //           .catch((err) => {
+      //             alert(err)
+      //           })
+      //       })
+      //   })
+      // })
+      // .catch((err) => {
+      //   liff
+      //     .sendMessages([
+      //       {
+      //         type: "text",
+      //         text: "Error...",
+      //       },
+      //       {
+      //         type: "text",
+      //         text: err.data.data.message,
+      //       },
+      //     ])
+      //     .then(() => {
+      //       console.log("message sent")
+      //       liff.closeWindow()
+      //     })
+      //     .catch((err) => {
+      //       alert(err)
+      //     })
+      // })
     })
     .catch((err) => {
       window.location = "./form.html"
