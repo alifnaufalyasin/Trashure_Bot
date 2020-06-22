84f54c4b197e810fbae4f80c874033aadf521749
alifnaufalyasin <alifnaufal171199@gmail.com>
2020-06-22 22:13:38 +0700

    Update LIFF for scan QR barang

--- liff/scanQR/liff-form.js
+++ liff/scanQR/liff-form.js
@@ -1,5 +1,5 @@
 window.onload = function () {
-  let myLiffId = "1654371439-2yo0m1Ag"
+  let myLiffId = "1654371439-ONAX01kB"
   initializeLiff(myLiffId)
 }
 
@@ -17,61 +17,60 @@ function initializeLiff(myLiffId) {
       let data = {}
       data.userId = profile.userId
       const dataToken = await axios({
-          url: "https://rpl-inventory.herokuapp.com/api/lineBot",
-          method: "GET",
-          data: data,
-        })
-      liff.scanCode().then(async result => {
+        url: "https://rpl-inventory.herokuapp.com/api/lineBot",
+        method: "GET",
+        data: data,
+      })
+      liff.scanCode().then(async (result) => {
         const kode = result.value
-        const data2 = {kode}
+        const data2 = { kode }
         axios({
           url: "https://rpl-inventory.herokuapp.com/api/scanQR",
           method: "POST",
           headers: {
-            'Authorization': `Bearer ${dataToken.data.data.token}`
+            Authorization: `Bearer ${dataToken.data.data.token}`,
           },
           data: data2,
         })
-        .then(async (response) => {
-          liff
-            .sendMessages([
-              {
-                type: "text",
-                text: "Berhasil scan barang "+response.data.data.nama,
-              },
-            ])
-            .then(() => {
-              console.log("message sent")
-              liff.closeWindow()
-            })
-            .catch((err) => {
-              alert(err)
-            })
-        })
-        .catch((err) => {
-          liff
-            .sendMessages([
-              {
-                type: "text",
-                text: "Error...",
-              },
-              {
-                type: "text",
-                text: err.data.data.message,
-              },
-            ])
-            .then(() => {
-              console.log("message sent")
-              liff.closeWindow()
-            })
-            .catch((err) => {
-              alert(err)
-            })
-        })
+          .then(async (response) => {
+            liff
+              .sendMessages([
+                {
+                  type: "text",
+                  text: "Berhasil scan barang " + response.data.data.nama,
+                },
+              ])
+              .then(() => {
+                console.log("message sent")
+                liff.closeWindow()
+              })
+              .catch((err) => {
+                alert(err)
+              })
+          })
+          .catch((err) => {
+            liff
+              .sendMessages([
+                {
+                  type: "text",
+                  text: "Error...",
+                },
+                {
+                  type: "text",
+                  text: err.data.data.message,
+                },
+              ])
+              .then(() => {
+                console.log("message sent")
+                liff.closeWindow()
+              })
+              .catch((err) => {
+                alert(err)
+              })
+          })
       })
     })
     .catch((err) => {
       window.location = "./form.html"
     })
 }
-
